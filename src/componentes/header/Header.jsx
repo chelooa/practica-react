15bd454cf1ad5d03f4f5bc069647a5137645d977
Marcelo <theg119.g@gmail.com>
2025-01-19 17:36:21 -0600

    fix: correct image source path in Header component

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css"
 
 function Header() {
   return  <header className="header">
-          <img src="public\https://raw.githubusercontent.com/chelooa/practica-react/refs/heads/main/public/Header%402x.png" alt="Header image" />
+          <img src="https://raw.githubusercontent.com/chelooa/practica-react/refs/heads/main/public/Header%402x.png" alt="Header image" />
           </header>
           
 }
